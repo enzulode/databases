92f1cc3c67e198c8c12b699459013bed3d76459e
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-02 01:58:56 +0300

    lab1 show script implemented

---
+++ lab1_show.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+SELECT * FROM location;
+SELECT * FROM person;
+SELECT * FROM ship_licence;
+SELECT * FROM ship_licence_registry;
+SELECT * FROM ship;
+SELECT * FROM ship_passport;
+SELECT * FROM interior_element;
+SELECT * FROM ship_interior_registry;
+SELECT * FROM ship_passengers_registry;
+
+
+COMMIT;
