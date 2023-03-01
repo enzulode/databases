8629b2fbd06fbfbe0190a031d8ba849dc2cd759c
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-01 23:49:31 +0300

    lab1 fill script update

--- lab1_fill.sql
+++ lab1_fill.sql
@@ -2,30 +2,18 @@ BEGIN;
 
 -- location table
 INSERT INTO location (description, x_coord, y_coord, z_coord)
-VALUES ('Диаспара', 1.0, 2.0, 3.0);
-
-INSERT INTO location (description, x_coord, y_coord, z_coord)
-VALUES ('Космос', 1.0, 2.0, 3.0);
-
-INSERT INTO location (description, x_coord, y_coord, z_coord)
-VALUES ('Море', 1.0, 2.0, 3.0);
-
-INSERT INTO location (description, x_coord, y_coord, z_coord)
-VALUES ('Океан', 1.0, 2.0, 3.0);
+VALUES ('Диаспара', 1.0, 2.0, 3.0),
+       ('Космос', 1.0, 2.0, 3.0),
+       ('Море', 1.0, 2.0, 3.0),
+       ('Океан', 1.0, 2.0, 3.0);
 
 
 -- person table
 INSERT INTO person (name, age, gender, location_id, character)
-VALUES ('Мастер', 42, 'М', 2, 'Сумасшедший');
-
-INSERT INTO person (name, age, gender, location_id, character)
-VALUES ('Олвин', 12, 'М', 1, 'Аскет');
-
-INSERT INTO person (name, age, gender, location_id, character)
-VALUES ('Иван', 12, 'М', 1, 'Аскет');
-
-INSERT INTO person (name, age, gender, location_id, character)
-VALUES ('Дима', 12, 'М', 1, 'Сумасшедший');
+VALUES ('Мастер', 42, 'М', 2, 'Сумасшедший'),
+       ('Олвин', 12, 'М', 1, 'Аскет'),
+       ('Иван', 12, 'М', 1, 'Аскет'),
+       ('Дима', 12, 'М', 1, 'Сумасшедший');
 
 
 -- ship license
@@ -49,28 +37,16 @@ VALUES (1, 1, 1);
 
 -- interior element
 INSERT INTO interior_element (name, type)
-VALUES ('дверь', 'декор');
-
-INSERT INTO interior_element (name, type)
-VALUES ('тарелка', 'посуда');
-
-INSERT INTO interior_element (name, type)
-VALUES ('домино', 'настолки');
+VALUES ('дверь', 'декор'),
+       ('тарелка', 'посуда'),
+       ('домино', 'настолки');
 
 -- interior elements registry
 INSERT INTO ship_interior_registry (ship_id, element_id)
-VALUES (1, 1);
-
-INSERT INTO ship_interior_registry (ship_id, element_id)
-VALUES (1, 2);
-
-INSERT INTO ship_interior_registry (ship_id, element_id)
-VALUES (1, 3);
-
-INSERT INTO ship_interior_registry (ship_id, element_id)
-VALUES (1, 2);
-
-INSERT INTO ship_interior_registry (ship_id, element_id)
-VALUES (1, 2);
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 2),
+       (1, 2);
 
 COMMIT;
